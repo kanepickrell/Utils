8deb4c074bd08abee036fb7ecbd8c5aa75d0977a
Your Name <you@example.com>
2022-11-27 23:11:10 -0500

    added passcode gen method

---
+++ passcoder.py
@@ -0,0 +1,26 @@
+#library reqs
+import random
+import string
+
+#plan:
+# create empty list, define password length, def passcoder() method
+
+word_length = 9
+
+components = [string.ascii_letters, string.digits, "!@#$%^&*"]
+chars = []
+
+for clist in components:
+    chars.append(clist)
+    for item in clist:
+        chars.append(item)
+
+def passcode_gen():
+    passcode = []
+    for i in range(word_length):
+        rchar = random.choice(chars)
+        passcode.append(rchar)
+
+    return "".join(passcode)
+
+print(passcode_gen())
